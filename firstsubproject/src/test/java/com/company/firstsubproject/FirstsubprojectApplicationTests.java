0a20274cb47551394a77b4cad14b33159d019d9d
Marvin Murage <Marvin.Murage@tracom.co.ke>
2022-07-13 15:03:13 +0300

    second commit

---
+++ firstsubproject/src/test/java/com/company/firstsubproject/FirstsubprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.firstsubproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstsubprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
